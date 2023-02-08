9bc05ce231dbd36480a38333db882657109ee090
YoussefElB <elb.youssef2000@gmail.com>
2023-02-08 14:56:28 +0100

    Added a controller with demo options

---
+++ server/ticktick/TickTick.App/Controllers/PersonsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TickTick.Models;
+
+namespace TickTick.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        static List<Person> people = new List<Person>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Person person = new Person("Yusup", "Elbu", "elmos@elm.os");
+            people.Add(person);
+            return Ok(person);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetId(Guid id)
+        {
+            var matchedPerson = people.Find(person => person.PublicId == id);
+            return Ok(matchedPerson);
+        }
+    }
+}
