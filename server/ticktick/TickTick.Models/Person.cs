1b30f090bfa571cef6e0a4ab3c9a27c87c8a2fac
YoussefElB <elb.youssef2000@gmail.com>
2023-02-08 14:18:38 +0100

    Initial commit

---
+++ server/ticktick/TickTick.Models/Person.cs
@@ -0,0 +1,48 @@
+﻿namespace TickTick.Models
+{
+    public class Person : BaseAuditableEntity, IEquatable<Person>
+    {
+        public Guid PublicId { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public string Email { get; set; }
+        public string? SocialSecurityNumber { get; set; }
+        public DateTime? DoB { get; set; }
+        public string? PhoneNumber { get; set; }
+        public Location? Address { get; set; }
+        public bool IsDeleted { get; private set; }
+
+        public Person(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+        public void Delete()
+        {
+            this.IsDeleted= true;
+        }
+        public void Update()
+        {
+            return;
+        }
+
+        public override string? ToString()
+        {
+            return $"{this.FirstName} {this.LastName} {this.Email}";
+        }
+
+        public bool Equals(Person? other)
+        {
+            if (!string.IsNullOrEmpty(this.SocialSecurityNumber) && !string.IsNullOrEmpty(other?.SocialSecurityNumber))
+            {
+                return this.SocialSecurityNumber == other.SocialSecurityNumber;
+            }
+            else
+            {
+                return this.PublicId == other?.PublicId;
+            }
+        }
+    }
+}
---
+++ server/ticktick/TickTick.Models/base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace TickTick.Models
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; set; }
+    }
+    public class BaseAuditableEntity : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
